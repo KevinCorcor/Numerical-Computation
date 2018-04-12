b55810f97bbd51e98d9b0eae754da489083682fd
KevinCorcor <corkeyin@gmail.com>
2018-04-12 10:21:31 +0100

    fixed to use correct roots

--- python/Ddiff.py
+++ python/Ddiff.py
@@ -1,46 +1,47 @@
 import numpy as np
-from sympy import Rational
+from sympy import *
 
 
-def DDiff(x,fx):
+def DDiff(xs,fxs):
     ''' newtons divided difference'''
     k = 1
-    while k < len(fx[0]):
+    while k < len(fxs[0]):
         n = k
         count = 0 # for counting how many calculations have been made on that trial
-        while n < len(fx):
-            if(count < len(fx[0])-k): #optimisation
+        while n < len(fxs):
+            if(count < len(fxs[0])-k): #optimisation
                # import ipdb; ipdb.set_trace()
-                fx[n][k] = Rational((fx[n+1][k-1]-fx[n-1][k-1]) ,(x[n+k][0]-x[n-k][0]))
+                fxs[n][k] = Rational((fxs[n+1][k-1]-fxs[n-1][k-1]) ,(xs[n+k][0]-xs[n-k][0]))
                 n += 2
                 count += 1
             else:
                 break #no need to make any more calculations for this trial
         k += 1
-    return (fx , np.diag(fx)) # return the upper diagonal
+    return (fxs , np.diag(fxs)) # return the upper diagonal
 
 
 x_in = np.asarray([[1],[2],[4]],Rational)     #seems to work regardless of input values
-x = np.asarray(np.zeros((2*len(x_in)-1,1)),Rational)
-x[::2] = x_in[:]                                    #add spacing
+xs = np.asarray(np.zeros((2*len(x_in)-1,1)),Rational)
+xs[::2] = x_in[:]                                    #add spacing
 
 fx_in = np.asarray([[0],[1],[2]],Rational)
-fx = np.asarray(np.zeros((2*len(fx_in)-1,len(fx_in))),Rational)
-fx[::2,0][:] = fx_in[:].T                              #add spacing
+fxs = np.asarray(np.zeros((2*len(fx_in)-1,len(fx_in))),Rational)
+fxs[::2,0][:] = fx_in[:].T                              #add spacing
 
 #CALCULATE POLYNOMIAL AND SIMPLIFY
-def poly(diag):
+def poly(xs,diag):
+    xs = xs[~np.all(xs == 0, axis=1)] # remove zero rows
+
     x = Symbol('x')
     f = diag[0]
 
-    for d in range(len(diag)-1,0,-1):
-        c=d
+    for d in range(1,len(diag)):
+        c=d-1
         r=1
-        while c>0:
-            r = r*(x-c)                    #any linear function
-
+        while c>=0:
+            r = r*(x-xs[c])                    #any linear function
             c = c-1
-        print(r)
+        #print(r)
         f = f + diag[d]*r
     f=simplify(f)
     return f
